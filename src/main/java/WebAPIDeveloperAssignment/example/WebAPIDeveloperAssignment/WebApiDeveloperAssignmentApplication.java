2638c72c199fc32d89e4c09cfc13c02f15b3cc00
wisevaishu <vpm.win@gmail.com>
2025-10-01 13:25:25 -0500

    First commit

---
+++ src/main/java/WebAPIDeveloperAssignment/example/WebAPIDeveloperAssignment/WebApiDeveloperAssignmentApplication.java
@@ -0,0 +1,13 @@
+package WebAPIDeveloperAssignment.example.WebAPIDeveloperAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiDeveloperAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiDeveloperAssignmentApplication.class, args);
+	}
+
+}
